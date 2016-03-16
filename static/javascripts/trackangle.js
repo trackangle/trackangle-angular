675115171a9054bf5814e4ec30dd67ff5b69d567
Ersan Zorlu <ersan.zorlu@comodo.com>
2016-03-16 17:39:15 +0200

    httpProvider is added to trackangle module

--- static/javascripts/trackangle.js
+++ static/javascripts/trackangle.js
@@ -2,24 +2,32 @@ define(['angularAMD', 'angular-route'], function (angularAMD) {
     'use strict';
 
   var trackangle = angular.module("trackangle", ['ngRoute']);
-  trackangle.config(function ($routeProvider, $locationProvider) {
-    /*$http.defaults.xsrfHeaderName = 'X-CSRFToken';
-	$http.defaults.xsrfCookieName = 'csrftoken';*/
+  trackangle.config(function ($routeProvider, $locationProvider, $httpProvider) {
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+	$httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('!');
 
     $routeProvider
     .when("/register", angularAMD.route({
-        templateUrl: '/static/javascripts/angular/authentication/templates/register.html', controller: 'RegisterController', controllerUrl: '/static/javascripts/angular/authentication/controllers/register.controller.js'
+        templateUrl: '/static/javascripts/angular/authentication/templates/register.html',
+        controller: 'RegisterController',
+        controllerUrl: '/static/javascripts/angular/authentication/controllers/register.controller.js'
     }))
     .when("/routes", angularAMD.route({
-        templateUrl: '/static/javascripts/angular/route/templates/routes.html', controller: 'RoutesController', controllerUrl: '/static/javascripts/angular/route/controllers/routes.controller.js'
+        templateUrl: '/static/javascripts/angular/route/templates/routes.html',
+        controller: 'RoutesController',
+        controllerUrl: '/static/javascripts/angular/route/controllers/routes.controller.js'
     }))
     .when("/route/create", angularAMD.route({
-        templateUrl: '/static/javascripts/angular/route/templates/create_route.html', controller: 'CreateRouteController', controllerUrl: '/static/javascripts/angular/route/controllers/create.route.controller.js'
+        templateUrl: '/static/javascripts/angular/route/templates/create_route.html',
+        controller: 'CreateRouteController',
+        controllerUrl: '/static/javascripts/angular/route/controllers/create.route.controller.js'
     }))
     .when("/route/:id", angularAMD.route({
-        templateUrl: '/static/javascripts/angular/route/templates/route.html', controller: 'RouteController', controllerUrl: '/static/javascripts/angular/route/controllers/route.controller.js'
+        templateUrl: '/static/javascripts/angular/route/templates/route.html',
+        controller: 'RouteController',
+        controllerUrl: '/static/javascripts/angular/route/controllers/route.controller.js'
     }))
     .otherwise({redirectTo: "/routes"});
   });
