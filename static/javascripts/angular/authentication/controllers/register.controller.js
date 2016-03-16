eb329e00ac4063599c7b2abde221db78b5ad17ba
Ersan Zorlu <ersan.zorlu@comodo.com>
2016-03-16 16:59:37 +0200

    Registration bug fix

--- static/javascripts/angular/authentication/controllers/register.controller.js
+++ static/javascripts/angular/authentication/controllers/register.controller.js
@@ -1,13 +1,10 @@
 
 define(['app', '/static/javascripts/angular/authentication/services/authentication.service.js'], function (app) {
     app.register.controller('RegisterController', ['$scope','Authentication', function ($scope, Authentication){
-    var vm = this;
 
-    vm.register = register;
-
-    function register() {
+    $scope.register = function() {
       console.log("asdasdasd");
-      Authentication.register(vm.email, vm.password, vm.username);
+      Authentication.register($scope.email, $scope.password, $scope.username);
     }
   }])
 });
