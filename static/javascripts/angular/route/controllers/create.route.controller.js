6eb4f577ff2f72740a61de2760d7565cc0da28b9
Ersan Zorlu <ersanvuralzorlu@gmail.com>
2016-05-01 17:29:59 +0300

    rebase with master branch

--- static/javascripts/angular/route/controllers/create.route.controller.js
+++ static/javascripts/angular/route/controllers/create.route.controller.js
@@ -1,62 +1,136 @@
 define(['trackangle', '/static/javascripts/angular/route/services/route.service.js', 'ngAutocomplete'], function (trackangle) {
     trackangle.register.controller('CreateRouteController', ['$scope','RouteService', function ($scope, RouteService){
 
-      console.log("Second controller");
-
-      $scope.result_city = '';
+       /*create route page navbar fuctions*/
+      var selected = this;
+      $scope.set_right_navbar = function (asd) {
+        selected.li = asd;
+      };
+      $scope.get_partial = function (){
+        return "/static/javascripts/angular/route/templates/"+selected.li+".html";
+      };
+      $scope.get_right_navbar = function (){
+        return selected.li;
+      };
+        /*create route page adding new place*/
+      $scope.choices = [{id: 'choice1'}];
+      $scope.addNewChoice = function() {
+        var newItemNo = $scope.choices.length+1;
+        $scope.choices.unshift({'id':'choice'+newItemNo});
+      };
+        /*create route page tab changing buttons*/
+      $scope.previous_tab = function() {
+        if($scope.get_right_navbar() == "food"){
+          $scope.set_right_navbar("accomodation");
+        }else if($scope.get_right_navbar() == "nightlife"){
+          $scope.set_right_navbar("food");
+        }else if($scope.get_right_navbar() == "entertainment_arts"){
+          $scope.set_right_navbar("nightlife");
+        }else if($scope.get_right_navbar() == "architecture_buildings"){
+          $scope.set_right_navbar("entertainment_arts");
+        }else if($scope.get_right_navbar() == "outdoor"){
+          $scope.set_right_navbar("architecture_buildings");
+        }
+      };
+      $scope.next_tab = function() {
+        if($scope.get_right_navbar() == "accomodation"){
+          $scope.set_right_navbar("food");
+        }else if($scope.get_right_navbar() == "food"){
+          $scope.set_right_navbar("nightlife");
+        }else if($scope.get_right_navbar() == "nightlife"){
+          $scope.set_right_navbar("entertainment_arts");
+        }else if($scope.get_right_navbar() == "entertainment_arts"){
+          $scope.set_right_navbar("architecture_buildings");
+        }else if($scope.get_right_navbar() == "architecture_buildings"){
+          $scope.set_right_navbar("outdoor");
+        }
+      };
+        /*Route Essentials*/
+      $scope.title = { route_title: "" };
+      $scope.description = { route_description: "" };
+      $scope.result_city = { route_result: "" };
       $scope.options_city =
       {
         watchEnter: true,
         types: '(cities)'
       };
-      $scope.details_city = '';
+      $scope.details_city = { route_details: "" };
 
-      $scope.result_museum = '';
-      $scope.options_museum = {};
-      $scope.details_museum = '';
-
-      $scope.result_food = '';
+        /* Fodd Details*/
+      $scope.result_food = { food_result: "" };
       $scope.options_food = {};
-      $scope.details_food = '';
+      $scope.details_food = { food_details: "" };
+      $scope.food_star_rating = { star_rating: ""};
+      $scope.food_budget = {budget: ""};
+      $scope.food_comment = {comment: ""};
 
-      $scope.result_shop = '';
-      $scope.options_shop = {};
-      $scope.details_shop = '';
+        /*Nightlife Details*/
+      $scope.result_nightlife = { nightlife_result: "" };
+      $scope.options_nightlife = {};
+      $scope.details_nightlife = { nightlife_details: "" };
+      $scope.nightlife_star_rating = { star_rating: ""};
+      $scope.nightlife_budget = {budget: ""};
+      $scope.nightlife_comment = {comment: ""};
 
-      $scope.$watch('details_city', function(){
-        if($scope.details_city) {
-          var country_index = $scope.details_city.address_components.length - 1;
-          $scope.options_museum = {
-               watchEnter: true,
-               types: 'establishment',
-               country: $scope.details_city.address_components[country_index].short_name.toLowerCase()
+        /*Entertainment and Arts Details*/
+      $scope.result_entertainment_arts = { entertainment_arts_result: "" };
+      $scope.options_entertainment_arts = {};
+      $scope.details_entertainment_arts = { entertainment_arts_details: "" };
+      $scope.entertainment_arts_star_rating = { star_rating: ""};
+      $scope.entertainment_arts_budget = {budget: ""};
+      $scope.entertainment_arts_comment = {comment: ""};
+
+        /*Architecture and Buildings Details */
+      $scope.result_architecture_buildings = { architecture_buildings_result: "" };
+      $scope.options_architecture_buildings = {};
+      $scope.details_architecture_buildings = { architecture_buildings_details: "" };
+      $scope.architecture_buildings_star_rating = { star_rating: ""};
+      $scope.architecture_buildings_budget = {budget: ""};
+      $scope.architecture_buildings_comment = {comment: ""};
+
+        /*Outdoor Details*/
+      $scope.result_outdoor = { outdoor_result: "" };
+      $scope.options_outdoor = {};
+      $scope.details_outdoor = { outdoor_details: "" };
+      $scope.outdoor_star_rating = { star_rating: ""};
+      $scope.outdoor_budget = {budget: ""};
+      $scope.outdoor_comment = {comment: ""};
+
+      $scope.$watch('details_city.route_details', function(){
+        if($scope.details_city.route_details) {
+          var country_index = $scope.details_city.route_details.address_components.length - 1;
+            $scope.options_food = {
+            watchEnter: true,
+            type: 'establishment',
+            country: $scope.details_city.route_details.address_components[country_index].short_name.toLowerCase()
           };
-          $scope.options_food = {
-               watchEnter: true,
-               types: 'establishment',
-               country: $scope.details_city.address_components[country_index].short_name.toLowerCase()
-          }
-          $scope.options_shop = {
-               watchEnter: true,
-               types: 'establishment',
-               country: $scope.details_city.address_components[country_index].short_name.toLowerCase()
-          }
         }
       });
-
-
       $scope.saveRoute = function() {
-
+          //console.log($scope.details_city);
+          //console.log($scope.details_food.food_details);
+          //console.log($scope.food_star_rating.star_rating);
+          //console.log($scope.food_budget.budget);
+          //console.log($scope.food_comment.comment);
             var places = [];
-            places.push({id: $scope.details_shop.place_id, type:0, location_lat:$scope.details_shop.geometry.location.lat(), location_lng:$scope.details_shop.geometry.location.lng()});
-            places.push({id: $scope.details_museum.place_id, type:1, location_lat:$scope.details_museum.geometry.location.lat(), location_lng:$scope.details_museum.geometry.location.lng()});
-            places.push({id: $scope.details_food.place_id, type:2, location_lat:$scope.details_food.geometry.location.lat(), location_lng:$scope.details_food.geometry.location.lng()});
+            places.push({
+              id: $scope.details_food.food_details.place_id,
+              type:0,
+              location_lat:$scope.details_food.food_details.geometry.location.lat(),
+              location_lng:$scope.details_food.food_details.geometry.location.lng(),
+              rating: $scope.food_star_rating.star_rating,
+              budget: $scope.food_budget.budget,
+              comment: $scope.food_comment.comment
+            });
+            //places.push({id: $scope.details_museum.place_id, type:1, location_lat:$scope.details_museum.geometry.location.lat(), location_lng:$scope.details_museum.geometry.location.lng()});
+            //places.push({id: $scope.details_food.place_id, type:2, location_lat:$scope.details_food.geometry.location.lat(), location_lng:$scope.details_food.geometry.location.lng()});
             var routeJSON = {
-                title: $scope.title,
-                description: $scope.description,
+                title: $scope.title.route_title,
+                description: $scope.description.route_description,
                 url_title: 'url_title',
                 places: places
             }
+            console.log(routeJSON);
             RouteService.createRoute(routeJSON).then(function(res) {
                 console.log("Res:"+ res.config.data.id);
                 console.log("Res:"+ res.config.data.title);
